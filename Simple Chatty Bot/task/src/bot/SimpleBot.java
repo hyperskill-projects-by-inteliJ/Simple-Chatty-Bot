e722b453233f115d0a57ee8e5b68261d717eec89
Manoj R. Randeni <randeni.ramr@gmail.com>
2022-06-04 17:35:17 +0530

    print numbers

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -15,11 +15,22 @@ public class SimpleBot {
         System.out.println("What a great name you have, " + name + "!");
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
-        int remainder3 = scanner.nextInt();
-        int remainder5 = scanner.nextInt();
-        int remainder7 = scanner.nextInt();
 
-        int userAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
-        System.out.printf("Your age is %d; that's a good time to start programming!", userAge);
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
+
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+
+        int numInput = scanner.nextInt();
+
+        for (int i = 0; i <= numInput; i++) {
+            System.out.println(i + "!");
+        }
+
+        System.out.println("Completed, have a nice day!");
     }
 }
