b0149fdbd17d1a0bc954852b2b9279a3b4e6b9a9
Manoj R. Randeni <randeni.ramr@gmail.com>
2022-06-04 17:26:06 +0530

    ask for username

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -1,7 +1,16 @@
 package bot;
 
+import java.util.Scanner;
+
 public class SimpleBot {
     public static void main(String[] args) {
-        // write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.println("Please, remind me your name.");
+        String userName = scanner.nextLine();
+
+        System.out.printf("What a great name you have, %s!", userName);
     }
 }
